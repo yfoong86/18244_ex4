d801253db6a9c4630e329e9ef9ca250afe445457
Yansi Foong <yansifoong@yansis-mbp.wifi.local.cmu.edu>
2026-02-21 23:00:16 -0500

    adding RangeFinder

--- docs/info.md
+++ docs/info.md
@@ -8,13 +8,31 @@ You can also include images in this folder and reference them in the markdown. E
 -->
 
 ## How it works
+The module will take in a go signal, from which it will start recognizing data on that clock edge.
+Then it will keep reading data until finish is asserted, and output what the range
+between the highest and lowest numbers are.
+
+There will also be an error signal which will assert for the following reasons:
+    - if go and finish are asserted together
+    - if go is asserted twice
+    - if finish is asserted before go
 
 Explain how your project works
+The project works using an FSM to keep track of if we are receiving values or if there is an error.
+There is also combinational logic in order to include the value currently on the clock cycle
+when finish is asserted.
 
 ## How to test
+The module can be tested with a testbench that has a few valid and invalid
+instructions. You can assert go, put a few values into data_in and check if the 
+range is valid when finish is asserted. You can also try a few invalid
+sequences to see if the error flag is working correctly.
 
 Explain how to use your project
+You can use the project to find the range between numbers that are being sent
+into a module one at a time.
 
 ## External hardware
+No external hardware is needed for the project.
 
 List external hardware used in your project (e.g. PMOD, LED display, etc), if any
--- src/project.v
+++ src/project.v
@@ -21,7 +21,110 @@ module tt_um_example (
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+  logic [15:0] data_in, range;
+  logic go, finish, error;
+
+
+  RangeFinder rf0(.clock(clk), .reset(~rst_n), 
+                  .data_in, .go, .finish, .range, .error);
+
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, 1'b0};
 
 endmodule
+
+`default_nettype none
+
+module RangeFinder
+   #(parameter WIDTH=16)
+    (input  logic [WIDTH-1:0] data_in,
+     input  logic             clock, reset,
+     input  logic             go, finish,
+     output logic [WIDTH-1:0] range,
+     output logic             error);
+
+   enum logic [1:0] {IDLE, RECEIVING, DONE, ERROR} currState, nextState;
+
+   logic [WIDTH-1:0] max, min, reg_max, reg_min;
+   logic [WIDTH-1:0] final_max, final_min;
+
+   // FSM next currState logic
+   always_comb begin
+      reg_min = min;
+      reg_max = max;
+      error = 1'b0;
+      case (currState)
+         IDLE: begin
+            if (go & finish) begin
+               nextState = ERROR;
+            end
+            else if (go) begin
+               nextState = RECEIVING;
+               reg_max = data_in;
+               reg_min = data_in;
+            end
+            else if (finish) begin
+               nextState = ERROR;
+            end
+            else nextState = IDLE;
+         end
+         RECEIVING: begin
+            if (go & finish)       nextState = ERROR;
+            else if (finish) begin
+               nextState = DONE;
+               if (data_in > max) reg_max = data_in;
+               if (data_in < min) reg_min = data_in;
+            end
+            else begin
+               nextState = RECEIVING;
+               if (data_in > max) reg_max = data_in;
+               if (data_in < min) reg_min = data_in;
+            end
+         end
+         DONE: begin
+            if (go & finish)       nextState = ERROR;
+            else if (go) begin
+               nextState = RECEIVING;
+               reg_max = data_in;
+               reg_min = data_in;
+            end
+            else if (finish)       nextState = ERROR;
+            else nextState = DONE;
+         end
+         ERROR: begin
+            error = 1'b1;
+            if (go & ~finish) begin
+               nextState = RECEIVING;
+               reg_max = data_in;
+               reg_min = data_in;
+            end
+            else                   nextState = ERROR;
+         end
+         default: nextState = IDLE;
+      endcase
+   end
+
+   always_ff @(posedge clock, posedge reset) begin
+      if (reset) currState <= IDLE;
+      else       currState <= nextState;
+   end
+
+   // Max/min registers
+   always_ff @(posedge clock, posedge reset) begin
+      if (reset) begin
+         max <= 'd0;
+         min <= 'd0;
+      end
+      else begin
+         max <= reg_max;
+         min <= reg_min;
+      end
+   end
+
+   always_comb begin
+      final_max = (data_in > max) ? data_in : max;
+      final_min = (data_in < min) ? data_in : min;
+      range = final_max - final_min;
+   end
+
+endmodule: RangeFinder
